85fc4362e66d6c175263e1c3d3c6fdcc656bace2
ahns0206 <ahns0206@gmail.com>
2022-04-11 00:13:20 +0900

    style : 주석 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     // save, findById, findAll는 Spring Data Jpa가 기본 제공함
+    // QuerydslPredicateExecutor는 실무에서 사용하기엔 한계가 있기에 상속받을지 고려 필수
 
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
